6ac0150567fcb5345ff0848f4b7257d3bd1d8b8c
Mereilim <ameekah93@gmail.com>
2019-11-26 11:30:38 -0500

    fix client package.json

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(express.json());
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+// Set static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+// }
 
 const PORT = process.env.PORT || 3100;
 
