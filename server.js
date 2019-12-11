a62896a59dccda6a58b45a9cb27be417f81e76da
Mereilim <ameekah93@gmail.com>
2019-12-11 11:41:57 -0500

    Connected to DB

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const connectDB = require("./config/db");
+
+//Connect DB
+connectDB();
 
 app.use(express.json());
 
