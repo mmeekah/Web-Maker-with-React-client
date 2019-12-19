899048e537d06c20316f8a3bb051a830d1917784
Mereilim <ameekah93@gmail.com>
2019-12-19 13:15:42 -0500

    Heroku+MLab+node.js

--- models/Page.js
+++ models/Page.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const PageSchema = mongoose.Schema({
   websiteId: { type: mongoose.Schema.Types.ObjectId, ref: "Website" },
-  name: { type: String, require: true },
-  title: { type: String, require: true }
+  name: { type: String, required: true },
+  title: { type: String }
 });
 
 module.exports = mongoose.model("Page", PageSchema);
